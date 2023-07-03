676ebf80150559056bac5a2d20236162b4ff51d9
Bruno <bvaleiro@gmail.com>
2023-07-03 19:44:44 -0300

    feature:push notifications working

--- App.tsx
+++ App.tsx
@@ -1,17 +1,34 @@
 import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
+import OneSignal from 'react-native-onesignal';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-
 import { Routes } from './src/routes';
-
 import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
-
 import { CartContextProvider } from './src/contexts/CartContext';
+import { tagUserInfoCreate } from './src/notifications/notificationsTags';
+import { useEffect } from 'react';
+OneSignal.setAppId('04088d57-84cd-4395-8803-f739017d1218');
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
+  tagUserInfoCreate()
+  useEffect(() => {
+    const unsubscrible = OneSignal.setNotificationOpenedHandler((response) => {
+      const { actionId } = response.action as any
+      switch (actionId) {
+        case '1':
+          return console.log('Ver todas');
+        case '2':
+          return console.log('Ver pedido');
+        default:
+          return console.log('Não foi clicado em batão de ação');
+
+      }
+    })
+    return () => unsubscrible
+  }, [])
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar
@@ -22,6 +39,7 @@ export default function App() {
       <CartContextProvider>
         {fontsLoaded ? <Routes /> : <Loading />}
       </CartContextProvider>
+
     </NativeBaseProvider>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,32 @@
+import { useState, useEffect } from 'react'
 import { useTheme } from 'native-base';
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 
 import { AppRoutes } from './app.routes';
+import OneSignal, { NotificationReceivedEvent, OSNotification } from 'react-native-onesignal';
+import { Notification } from '../components/Notification';
 
 export function Routes() {
   const { colors } = useTheme();
-
+  const [notification, setNotification] = useState<OSNotification>()
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
-
+  useEffect(() => {
+    const unsubscribe = OneSignal.setNotificationWillShowInForegroundHandler((
+      notificationReceivedEvent: NotificationReceivedEvent
+    ) => {
+      const response = notificationReceivedEvent.getNotification()
+      setNotification(response) // see the payload sent from your server
+    })
+    return () => unsubscribe
+  }, [])
   return (
     <NavigationContainer theme={theme}>
       <AppRoutes />
+      {
+        notification?.title &&
+        <Notification data={notification} onClose={() => setNotification(undefined)} />
+      }
     </NavigationContainer>
   );
 }
